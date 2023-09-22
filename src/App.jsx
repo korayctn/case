fb5d2ca507ba7c1a1171120cb68e1080c24ae342
korayctn <korayctnx06@gmail.com>
2023-09-22 21:12:10 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="main">
+      <h2>Deneme</h2>
+    </div>
+  );
+}
+
+export default App;
